07578aa22a4f93f03fa3fcff4211e73da1d0ebdc
zhujikuan <1335289286@qq.com>
2022-10-28 14:33:56 +0800

    fprintf写文件记录日志

--- log.h
+++ log.h
@@ -13,3 +13,10 @@
         dup2(log_fd, 1);                                               \
         dup2(log_fd, 2);                                               \
     } while (0);
+
+#define LOG_FPRINTF_TO_FILE(format, ...)             \
+    do {                                             \
+        FILE *log_file = fopen(LOG_FILE_PATH, "a+"); \
+        fprintf(log_file, format, ##__VA_ARGS__);    \
+        fclose(log_file);                            \
+    } while (0);
--- test_log.c
+++ test_log.c
@@ -1,6 +1,9 @@
-#include"log.h"
+#include "log.h"
+
 int main() {
-    LOG_OPEN_REDIRECT();
+    // LOG_OPEN_REDIRECT();
+    // LOG_FPRINTF_TO_FILE("%d:%s\n", 9999, "zhujikuan");
+
     printf("hello\n");
     printf("hello test log\n");
     return 0;
